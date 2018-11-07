6baedc8328504290c60a0c416e800f108b551986
Mariia Charikova <m.charikova@innopolis.ru>
2018-11-07 22:14:38 +0300

    Create tables for all entities (without relationships yet)

--- main.py
+++ main.py
@@ -2,26 +2,75 @@ import sqlite3
 db = sqlite3.connect('mydb')
 
 cursor = db.cursor()
-# example how to create tables
-# cursor.execute('''
-#         CREATE TABLE customers (cid INTEGER PRIMARY KEY,
-#                                 username VARCHAR(30) NOT NULL,
-#                                 full_name VARCHAR(40),
-#                                 location VARCHAR(40),
-#                                 phone_number VARCHAR(11),
-#                                 email VARCHAR(40))
-# ''')
 
-username1 = 'name1'
-full_name1 ='fullname1'
-location1 = 'location1'
-phone_number1 ='8805553535'
-email1 = 'email1'
+cursor.execute('''
+    CREATE TABLE customers (
+        cid INTEGER PRIMARY KEY,                                 
+        username VARCHAR(30) NOT NULL,                                 
+        full_name VARCHAR(40),
+        location VARCHAR(40),
+        phone_number VARCHAR(11),
+        email VARCHAR(40))                              
+''')
 
-#how to insert info
-# Insert cust 1
+cursor.execute('''
+    CREATE TABLE rides (
+        rid INTEGER PRIMARY KEY,
+        initial_location VARCHAR(40),
+        destination VARCHAR(40))                            
+''')
+
+cursor.execute('''
+    CREATE TABLE cars (
+        carid INTEGER PRIMARY KEY,
+        current_state VARCHAR(20),
+        battery_level INTEGER,
+        location VARCHAR(40))                          
+''')
+
+cursor.execute('''
+    CREATE TABLE plug_types (
+        ptid INTEGER PRIMARY KEY,
+        shape VARCHAR(20),
+        size INTEGER)                        
+''')
+
+cursor.execute('''
+    CREATE TABLE charging_stations (
+        csid INTEGER PRIMARY KEY,
+        location VARCHAR(40),
+        price INTEGER,
+        plug_types VARCHAR(40),
+        time_of_charging_min INTEGER,
+        amount_of_sockets INTEGER)                     
+''')
+
+cursor.execute('''
+    CREATE TABLE car_part_types (
+        cpid INTEGER PRIMARY KEY,
+        price INTEGER)                   
+''')
+
+cursor.execute('''
+    CREATE TABLE providers (
+        pid INTEGER PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        phone_number VARCHAR(11),
+        location VARCHAR(40))                
+''')
+
+cursor.execute('''
+    CREATE TABLE workshops (
+        wid INTEGER PRIMARY KEY,
+        name VARCHAR(20),
+        location VARCHAR(40),
+        availability VARCHAR(20),
+        price_in_hours INTEGER)             
+''')
+
+
 cursor.execute('''INSERT INTO customers(username, full_name, location, phone_number, email)
-                  VALUES(?,?,?,?,?)''', (username1, full_name1,location1, phone_number1, email1))
+                  VALUES(username1, full_name1,location1, 88005553535, email1)''')
 print('First user inserted')
 
 db.commit()

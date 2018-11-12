f81956447a90b4b9a81e4f2d5feb625514b50e58
Danil Ginzburg <danilgin@inbox.ru>
2018-11-12 21:27:47 +0300

    found bugs

--- db_structure.py
+++ db_structure.py
@@ -6,48 +6,46 @@ db = Database('db.sqlite')
 
 db.exec("PRAGMA foreign_keys=ON")
 db.exec('''
-    CREATE TABLE customers (
+    CREATE TABLE IF NOT EXISTS customers (
         cid INTEGER PRIMARY KEY,                                 
-        username VARCHAR(30) NOT NULL,                                 
+        username VARCHAR(30) UNIQUE NOT NULL,                                 
         full_name VARCHAR(40),
         location VARCHAR(40),
         phone_number VARCHAR(11),
-        email VARCHAR(40)
-        )                              
+        email VARCHAR(40))                              
 ''')
 
 db.exec('''
-    CREATE TABLE rides (
+    CREATE TABLE IF NOT EXISTS rides (
         rid INTEGER PRIMARY KEY,
         initial_location VARCHAR(40),
         destination VARCHAR(40),
-        cid INTEGER UNIQUE, # one-to-one connection to customer
-        FOREIGN KEY (cid) references customers(cid),
-        carid INTEGER UNIQUE , # one-to-one connection to car
-        FOREIGN KEY (carid) references cars(carid)
-        )                            
+        carid INTEGER UNIQUE, 
+        cid INTEGER UNIQUE,
+        FOREIGN KEY (carid) REFERENCES cars(carid),
+        FOREIGN KEY (cid) REFERENCES customers(cid)
+        )
 ''')
 
 db.exec('''
-    CREATE TABLE cars (
+    CREATE TABLE IF NOT EXISTS cars (
         carid INTEGER PRIMARY KEY,
         current_state VARCHAR(20),
         battery_level INTEGER,
-        location VARCHAR(40))                          
+        location VARCHAR(40), 
+        ptid INTEGER,
+        FOREIGN KEY (ptid) REFERENCES plug_types(ptid))                          
 ''')
 
 db.exec('''
-    CREATE TABLE plug_types (
+    CREATE TABLE IF NOT EXISTS plug_types (
         ptid INTEGER PRIMARY KEY,
         shape VARCHAR(20),
-        size INTEGER,
-        carid INTEGER,
-        FOREIGN KEY (carid) REFERENCES cars(carid)
-        )                        
+        size INTEGER)                  
 ''')
 
 db.exec('''
-    CREATE TABLE charging_stations (
+    CREATE TABLE IF NOT EXISTS charging_stations (
         csid INTEGER PRIMARY KEY,
         location VARCHAR(40),
         price INTEGER,
@@ -57,13 +55,13 @@ db.exec('''
 ''')
 
 db.exec('''
-    CREATE TABLE car_part_types (
+    CREATE TABLE IF NOT EXISTS car_part_types (
         cpid INTEGER PRIMARY KEY,
         price INTEGER)                   
 ''')
 
 db.exec('''
-    CREATE TABLE providers (
+    CREATE TABLE IF NOT EXISTS providers (
         pid INTEGER PRIMARY KEY,
         name VARCHAR(20) NOT NULL,
         phone_number VARCHAR(11),
@@ -72,7 +70,7 @@ db.exec('''
 ''')
 
 db.exec('''
-    CREATE TABLE workshops (
+    CREATE TABLE IF NOT EXISTS workshops (
         wid INTEGER PRIMARY KEY,
         name VARCHAR(20),
         location VARCHAR(40),
@@ -81,28 +79,28 @@ db.exec('''
 ''')
 # ----MAKING MANY-TO-MANY RELATIONSHIPS----
 db.exec('''
-    CREATE TABLE workshops_have_car_parts (
+    CREATE TABLE IF NOT EXISTS workshops_have_car_parts (
     wid INTEGER, 
     cpid INTEGER
     )
 ''')
 
 db.exec('''
-    CREATE TABLE providers_provide_car_parts (
+    CREATE TABLE IF NOT EXISTS providers_provide_car_parts (
     pid INTEGER, 
     cpid INTEGER
     )
 ''')
 
 db.exec('''
-    CREATE TABLE cars_have_car_parts (
-    carid INTEGER, 
+    CREATE TABLE IF NOT EXISTS cars_have_car_parts (
+    carid INTEGER,
     cpid INTEGER
     )
 ''')
 
 db.exec('''
-    CREATE TABLE charging_stations_suits_plug_types (
+    CREATE TABLE IF NOT EXISTS charging_stations_suits_plug_types (
     ptid INTEGER, 
     csid INTEGER
     )

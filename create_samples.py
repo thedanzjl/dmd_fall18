d68cf4a8a5b76e23105817ff8dd25810c5736c84
Danil Ginzburg <danilgin@inbox.ru>
2018-11-15 18:59:19 +0300

    added samples for rides

--- create_samples.py
+++ create_samples.py
@@ -1,16 +1,19 @@
 import random
 import string
+from datetime import datetime
 from interface import Database
 
 db = Database("db.sqlite")
 db.clear_all()
 
 
-# INSERTING CUSTOMER'S SAMPLES
+# INSERTING customers SAMPLES
 N = 10
 
 names = ['Daniel', 'Roman', 'Mary', 'Nikolay', 'Vladimir']
 locations = ['Barbara street 1337', 'Universitetskaya 1', 'Sportivnaya 3']
+locations2 = ['Barbara street', 'Universitetskaya', 'Sportivnaya']
+
 
 chars = string.printable[:62]
 usernames = list()
@@ -30,43 +33,61 @@ for id in range(N):
     db.insert_into('customers', cid=id, username=username, full_name=full_name, location=location,
                 phone_number=phone_number, email=email)
 
-# INSERTING CARS SAMPLES
+# INSERTING cars SAMPLES
 
 
 colors = ['Red', 'Green', 'Blue', 'Black', 'White']
-locations2 = ['Barbara street', 'Universitetskaya', 'Sportivnaya']
 states = ['broken','awaiting','busy','charging']
 
 for id in range(N):
-    plate = ''.join([random.choice('0123456789') for i in range(5)])
+    plate = ''.join([random.choice('0123456789') for _ in range(5)])
     color = random.choice(colors)
     current_state = random.choice(states)
-    battery_level = ''.join([random.choice('0123456789') for i in range(2)])
-    location = str(random.choice(locations2)) + ''.join([random.choice('0123456789') for i in range(3)])
+    battery_level = ''.join([random.choice('0123456789') for _ in range(2)])
+    location = str(random.choice(locations2)) + ''.join([random.choice('0123456789') for _ in range(3)])
     ptid = ''.join([random.choice('0123456789') for i in range(2)])
 
     db.insert_into('cars', carid=id, plate=plate, color=color, current_state=current_state,
-                   battery_level = battery_level, location = location, ptid = ptid)
+                   battery_level=battery_level, location=location, ptid=ptid)
 
 
-# INSERTING RIDES SAMPLES
+# INSERTING rides SAMPLES
 
-# INSERTING PLUG_TYPES SAMPLES
+for id in range(N):
+    initial_location = random.choice(locations)
+    source_location = random.choice(locations)
+    destination = random.choice(locations)
+    start_ride_time = datetime(2018, 11, 16, random.randint(0, 1), random.randint(0, 59), 0)
+    end_ride_time = datetime(2018, 11, 16, random.randint(2, 3), random.randint(0, 59), 0)
+    carid = random.randint(0, N-1)
+    cid = random.randint(0, N-1)
 
-# INSERTING CHARGING_STATIONS SAMPLES
+    db.insert_into('rides', rid=id, initial_car_location=initial_location,
+                   source_location=source_location, destination=destination, cid=cid,
+                   start_ride_time=start_ride_time, end_ride_time=end_ride_time, carid=carid
+                   )
 
-# INSERTING CAR_PARTS SAMPLES
+# INSERTING plug_types SAMPLES
 
-# INSERTING PROVIDERS SAMPLES
+# INSERTING charging_stations SAMPLES
 
-# INSERTING WORKSHOPS SAMPLES
+# INSERTING car_parts SAMPLES
+
+# INSERTING providers SAMPLES
+
+# INSERTING workshops_sell_car_parts SAMPLES
+
+# INSERTING providers_provide_car_parts SAMPLES
+
+# INSERTING cars_repaired SAMPLES
+
+# INSERTING cars_have_car_parts SAMPLES
+
+# INSERTING charging_stations_suits_plug_types SAMPLES
+
+# INSERTING cars_charged SAMPLES
 
 
 del db
 
-#
-# if __name__ == '__main__':
-#     print(db.query("""
-#     SELECT * FROM customers
-#     """))
 

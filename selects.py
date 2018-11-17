1323a0b1f1fe0e471a30f9ce1ebd29f09d1a861c
Danil Ginzburg <danilgin@inbox.ru>
2018-11-17 17:46:30 +0300

    1 & 2 selects

--- selects.py
+++ selects.py
@@ -1,27 +1,45 @@
 """
 This file releases 2.3 goal in the assignment: Implement select queries
 """
-from interface import Database
+from datetime import datetime
+from interface import *
 db = Database('db.sqlite')
 
 
+@intro
 def select_3_1():
     """A customer claims she forgot her bag in a car and asks to help.
     She was using cars several times this day, but she believes the right
     car was red and its plate starts with “AN”. Find all possible cars
     that match the description."""
-    pass
+    return db.query('''SELECT carid FROM cars WHERE color="Red" and substr(plate, 1, 2)="AN"''')
 
 
+@intro
 def select_3_2():
     """
     Company management wants to get a statistics on the efficiency of
     charging stations utilization. Given a date, compute how many sockets
     were occupied each hour.
     """
-    pass
+    date = MyDate(2018, 11, 8)
+    occupied = db.query('''SELECT usage_time, charging_time_amount FROM cars_charged 
+                           WHERE date(usage_time)="{}"'''.format(str(date)))
+    hours = dict()
+    for hour in range(24):
+        hours_occupied = '{:02}h-{:02}h'.format(hour, hour+1)
+        hours[hours_occupied] = 0
+        for socket in occupied:
+            duration = socket[1]
+            h = int(socket[0][11:13])
+            m = int(socket[0][14:16])
+            if (h == hour) or (h == hour - 1 and m + duration >= 60):
+                hours[hours_occupied] += 1
+
+    return hours
 
 
+@intro
 def select_3_3():
     """
     Company management considers using price increasing coefficients.
@@ -32,6 +50,7 @@ def select_3_3():
     pass
 
 
+@intro
 def select_3_4():
     """
     A customer claims that he was charged twice for the trip, but he
@@ -39,9 +58,16 @@ def select_3_4():
     his phone and he is too lazy to ask the bank), so you need to check all
     his payments for the last month to be be sure that nothing was doubled.
     """
-    pass
+    # cid = 7  # consider customer
+    #     # pays_of_user = db.query('''SELECT paytime, amount FROM payments WHERE cid={}'''.format(cid))
+    #     # for i, pay1 in enumerate(pays_of_user):
+    #     #     for j, pay2 in enumerate(pays_of_user):
+    #     #         if i != j and pay1[0] == pay1[0] and pay2[0] == pay2[0]:
+    #     #             return 'User paid twice at ' + pay1[0]
+    #     # return 'User paid only once'
 
 
+@intro
 def select_3_5():
     """
     The department of development has requested the following statistics:
@@ -52,6 +78,7 @@ def select_3_5():
     pass
 
 
+@intro
 def select_3_6():
     """
     In order to accommodate traveling demand, the company decided to
@@ -62,6 +89,7 @@ def select_3_6():
     pass
 
 
+@intro
 def select_3_7():
     """
     Despite the wise management, the company is going through hard times
@@ -72,6 +100,7 @@ def select_3_7():
     pass
 
 
+@intro
 def select_3_8():
     """
     The company management decided to participate in the research on “does customer
@@ -82,6 +111,7 @@ def select_3_8():
     pass
 
 
+@intro
 def select_3_9():
     """
     The company management decided to optimize repair costs by buying parts in bulks
@@ -91,6 +121,7 @@ def select_3_9():
     pass
 
 
+@intro
 def select_3_10():
     """
     The company management decided to cut costs by getting rid of the most expensive
